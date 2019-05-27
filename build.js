2f5b44716f3b840e1624bd81092b08a6b7facd9b
Aleksandra Dworak <aaleksandra.dworak@gmail.com>
2019-05-27 12:20:56 +0200

    corections in setState functions

--- build.js
+++ build.js
@@ -2,11 +2,20 @@
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+/* setState przyjmuje obiekt jako argument. Twoj zapis (this.state = {...) 
+  jest problematyczny w zrozumieniu
+
+  tak samo w calculate nie modyfikuj this.state bezposrednio, 
+  tylko  przypisz to do zmiennych, zaktualizuj je i ustaw setState
+ */
 
 var Stopwatch = function (_React$Component) {
   _inherits(Stopwatch, _React$Component);
@@ -18,25 +27,22 @@ var Stopwatch = function (_React$Component) {
 
     _this.running = false;
     _this.state = {
-
       minutes: 0,
       seconds: 0,
       miliseconds: 0
     };
 
     _this.print(_this.state.minutes + _this.state.seconds + _this.state.miliseconds);
-
     _this.start = _this.start.bind(_this);
     _this.restart = _this.restart.bind(_this);
     _this.stop = _this.stop.bind(_this);
-
     return _this;
   }
 
   _createClass(Stopwatch, [{
     key: 'reset',
     value: function reset() {
-      this.setState(this.state = {
+      this.setState({
         minutes: 0,
         seconds: 0,
         miliseconds: 0
@@ -45,13 +51,11 @@ var Stopwatch = function (_React$Component) {
   }, {
     key: 'print',
     value: function print() {
-
       return this.format(this.state);
     }
   }, {
     key: 'format',
     value: function format(state) {
-
       return pad0(state.minutes) + ':' + pad0(state.seconds) + ':' + pad0(Math.floor(state.miliseconds));
     }
   }, {
@@ -59,7 +63,6 @@ var Stopwatch = function (_React$Component) {
     value: function start() {
       var _this2 = this;
 
-      console.log(this.state);
       if (!this.running) {
         this.running = true;
         this.watch = setInterval(function () {
@@ -72,25 +75,28 @@ var Stopwatch = function (_React$Component) {
     value: function step() {
       if (!this.running) return;
       this.calculate();
-      this.setState(this.state = {
-        minutes: this.state.minutes,
-        seconds: this.state.seconds,
-        miliseconds: this.state.miliseconds
-      });
       this.print();
     }
   }, {
     key: 'calculate',
     value: function calculate() {
-      this.state.miliseconds += 1;
-      if (this.state.miliseconds >= 100) {
-        this.state.seconds += 1;
-        this.state.miliseconds = 0;
+      var miliseconds = this.state.miliseconds;
+      var seconds = this.state.seconds;
+      var minutes = this.state.minutes;
+
+      miliseconds += 1;
+      if (miliseconds >= 100) {
+        seconds += 1;
+        miliseconds = 0;
       }
-      if (this.state.seconds >= 60) {
-        this.state.minutes += 1;
-        this.state.seconds = 0;
+      if (seconds >= 60) {
+        minutes += 1;
+        seconds = 0;
       }
+      this.setState(_defineProperty({
+        miliseconds: miliseconds,
+        seconds: seconds
+      }, 'miliseconds', miliseconds));
     }
   }, {
     key: 'restart',
@@ -101,14 +107,12 @@ var Stopwatch = function (_React$Component) {
   }, {
     key: 'stop',
     value: function stop() {
-
       this.running = false;
       clearInterval(this.watch);
     }
   }, {
     key: 'render',
     value: function render() {
-
       return React.createElement(
         'div',
         null,
--- script.js
+++ script.js
@@ -1,83 +1,88 @@
+/* setState przyjmuje obiekt jako argument. Twoj zapis (this.state = {...) 
+  jest problematyczny w zrozumieniu
+
+  tak samo w calculate nie modyfikuj this.state bezposrednio, 
+  tylko  przypisz to do zmiennych, zaktualizuj je i ustaw setState
+ */
 
 class Stopwatch extends React.Component {
   constructor() {
     super();
     this.running = false;
-    this.state= {
-      
+    this.state = {
       minutes: 0,
       seconds: 0,
       miliseconds: 0
     };
-    
-    this.print(this.state.minutes + this.state.seconds + this.state.miliseconds);
 
-   this.start = this.start.bind(this);
-   this.restart = this.restart.bind(this);
-   this.stop = this.stop.bind(this);
-  
+    this.print(this.state.minutes + this.state.seconds + this.state.miliseconds);
+    this.start = this.start.bind(this);
+    this.restart = this.restart.bind(this);
+    this.stop = this.stop.bind(this);
   }
 
   reset() {
-    this.setState(
-      this.state = {
-        minutes: 0,
-        seconds: 0,
-        miliseconds:0
-      });
+    this.setState({
+      minutes: 0,
+      seconds: 0,
+      miliseconds: 0
+    });
   }
+
   print() {
-    
-    
     return this.format(this.state);
   }
+
   format(state) {
-    
     return `${pad0(state.minutes)}:${pad0(state.seconds)}:${pad0(Math.floor(state.miliseconds))}`;
   }
+
   start() {
-    console.log(this.state)
     if (!this.running) {
       this.running = true;
       this.watch = setInterval(() => this.step(), 10);
-     
     }
   }
+
   step() {
     if (!this.running) return;
     this.calculate();
-    this.setState(
-      this.state = {
-        minutes: this.state.minutes,
-        seconds: this.state.seconds,
-        miliseconds:this.state.miliseconds
-      });
     this.print();
   }
+
   calculate() {
-    this.state.miliseconds += 1;
-    if (this.state.miliseconds >= 100) {
-      this.state.seconds += 1;
-      this.state.miliseconds = 0;
+    let miliseconds = this.state.miliseconds;
+    let seconds = this.state.seconds;
+    let minutes = this.state.minutes;
+
+    miliseconds += 1;
+    if (miliseconds >= 100) {
+      seconds += 1;
+      miliseconds = 0;
     }
-    if (this.state.seconds >= 60) {
-      this.state.minutes += 1;
-      this.state.seconds = 0;
+    if (seconds >= 60) {
+      minutes += 1;
+      seconds = 0;
     }
+    this.setState({
+      miliseconds: miliseconds,
+      seconds: seconds,
+      miliseconds: miliseconds
+    })
   }
+
   restart() {
     this.reset();
     this.print();
   }
+
   stop() {
-    
     this.running = false;
     clearInterval(this.watch);
   }
- 
-  render(){
-    
-    return(
+
+  render() {
+    return (
       <div>
         <nav className={'nav'}>
           <button onClick={this.start}>Start</button>
@@ -88,9 +93,9 @@ class Stopwatch extends React.Component {
         <ul className="results"></ul>
       </div>
     )
-	}
-
   }
+
+}
 
 function pad0(value) {
   let result = value.toString();
